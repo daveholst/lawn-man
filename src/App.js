ac381f069add28f562116c3622a50f3f2e94d59e
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-08-05 11:06:23 +0800

    setup apollo client, context and caching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import React from 'react';
+
+// import apolloClient
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import LandingPage from './components/LandingPage';
 
+// build graphQl endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Create apollo client
+const apolloClient = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 const App = () => (
-  <>
+  <ApolloProvider client={apolloClient}>
     <LandingPage />
-  </>
+  </ApolloProvider>
 );
 
 export default App;
