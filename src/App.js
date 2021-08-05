d2c073ee8977fb7d4f79bea74af13b29451108a2
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-08-05 21:25:15 +0800

    changed to internal docker uri for graph api

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ import Auth from './utils/authUtils';
 
 // build graphQl endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  // uri: '/graphql',
+  uri: 'http://lawn-man-server:5000/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
