94e70eac5480154fd7200442d1a2fe9536283ebd
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-08-05 22:05:42 +0800

    changed to internal docker uri for graph api - take5 - https error, cant use docker network - try fix later

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ import Auth from './utils/authUtils';
 // build graphQl endpoint
 const httpLink = createHttpLink({
   // uri: '/graphql',
-  // uri: 'https://lawn-man-server.holst.club/graphql',
-  uri: 'https://lawn-man-server.web:5000/graphql',
+  uri: 'https://lawn-man-server.holst.club/graphql',
+  // uri: 'https://lawn-man-server.web:5000/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
