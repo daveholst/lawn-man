9df5af6c0b1b787bb25bff8651fb676258f844ba
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-08-05 12:12:52 +0800

    user object printing to console on submit from form

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { makeStyles } from '@material-ui/core/styles';
-
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Auth from '../utils/authUtils';
 
+// styling
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
@@ -24,16 +25,60 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const SignUpForm = ({ handleClose }) => {
+  const [userFormData, setUserFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = async (e) => {
+    const { name, value } = e.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userFormData);
+    handleClose();
+  };
+
   SignUpForm.propTypes = {
     handleClose: PropTypes.func,
   };
   const classes = useStyles();
   return (
-    <form className={classes.root}>
-      <TextField label="First Name" variant="filled" required />
-      <TextField label="Last Name" variant="filled" required />
-      <TextField label="Email" variant="filled" type="email" required />
-      <TextField label="Password" variant="filled" type="password" required />
+    <form className={classes.root} onSubmit={handleSubmit}>
+      <TextField
+        label="First Name"
+        name="firstName"
+        variant="filled"
+        required
+        onChange={handleInputChange}
+      />
+      <TextField
+        label="Last Name"
+        name="lastName"
+        variant="filled"
+        required
+        onChange={handleInputChange}
+      />
+      <TextField
+        label="Email"
+        name="email"
+        variant="filled"
+        type="email"
+        required
+        onChange={handleInputChange}
+      />
+      <TextField
+        label="Password"
+        name="password"
+        variant="filled"
+        type="password"
+        required
+        onChange={handleInputChange}
+      />
       <div>
         <Button variant="contained" onClick={handleClose}>
           Cancel
