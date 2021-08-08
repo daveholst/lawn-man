fc0f8e7e628f5e2c2f53d670324ace42a0662699
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-08-08 17:11:28 +0800

    re-design with background image

---
+++ src/utils/mfrc-template.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+}));
+
+const ${1:componentName} = () => {
+  const classes = useStyles();
+  return (
+
+  );
+}
+
+export default ${1:componentName}
